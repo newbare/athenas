225513d65a5fa1ed897f9b64d56de359faad674a
Jefferson <jeferaleite@gmail.com>
2015-07-23 01:25:01 -0300

    Ajustes

--- athenas/src/main/java/as/athen/springmvc/repo/MemberDaoImpl.java
+++ athenas/src/main/java/as/athen/springmvc/repo/MemberDaoImpl.java
@@ -30,12 +30,7 @@ public class MemberDaoImpl implements MemberDao
     {
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<Member> criteria = builder.createQuery(Member.class);
-        Root<Member> member = criteria.from(Member.class);
-
-        /*
-         * Swap criteria statements if you would like to try out type-safe criteria queries, a new
-         * feature in JPA 2.0 criteria.select(member).orderBy(cb.asc(member.get(Member_.name)));
-         */
+        Root<Member> member = criteria.from(Member.class);  
 
         criteria.select(member).where(builder.equal(member.get("email"), email));
         return em.createQuery(criteria).getSingleResult();
@@ -46,12 +41,7 @@ public class MemberDaoImpl implements MemberDao
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Member> criteria = cb.createQuery(Member.class);
         Root<Member> member = criteria.from(Member.class);
-
-        /*
-         * Swap criteria statements if you would like to try out type-safe criteria queries, a new
-         * feature in JPA 2.0 criteria.select(member).orderBy(cb.asc(member.get(Member_.name)));
-         */
-
+        
         criteria.select(member).orderBy(cb.asc(member.get("name")));
         return em.createQuery(criteria).getResultList();
     }
